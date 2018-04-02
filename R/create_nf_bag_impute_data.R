357415cbfb83611badceb353593396193126470b
Timothy Mastny <tim.mastny@gmail.com>
2018-04-02 17:49:06 -0500

    added new function to create new features and improve current ones

---
+++ R/create_nf_bag_impute_data.R
@@ -0,0 +1,35 @@
+
+
+library(tidyverse)
+library(magrittr)
+library(recipes)
+library(here)
+
+source(here("R", "utilities.R"))
+
+train <- read_csv(here("data", "train.csv"))
+test <- read_csv(here("data", "test.csv"))
+
+train %<>%
+  unique_sessioner() %>%
+  fix_customerid()
+
+test %<>%
+  unique_sessioner() %>%
+  fix_customerid()
+
+bag_impute_rec <- recipe(order ~ ., data = train) %>%
+  step_modeimpute(status, availability) %>%
+  step_bagimpute(all_predictors(), -status, -availability, -customerID) %>%
+  step_dummy(status, availability) %>%
+  step_center(all_predictors(), -contains("status"), -contains("availability")) %>%
+  step_scale(all_predictors(), -contains("status"), -contains("availability")) %>%
+  prep(training = train)
+
+train <- bake(bag_impute_rec, newdata = train)
+test <- bake(bag_impute_rec, newdata = test)
+
+write.csv(train, here("data", "bagim-dcs-train.csv"))
+write.csv(test, here("data", "bagim-dcs-test.csv"))
+
+
--- R/utilities.R
+++ R/utilities.R
@@ -13,6 +13,28 @@ unique_sessioner <- function(data) {
     select(-row_id)
 }
 
+unique_sessioner2 <- function(data) {
+  data %>%
+    group_by(sessionID) %>%
+    mutate(row_id = row_number()) %>%
+    mutate(session_nums = max(row_id)) %>%
+    mutate_at(
+      vars(contains("Min")),
+      ~ifelse(all(is.na(.)), NA, min(., na.rm = TRUE))) %>%
+    mutate(min_cartTotal = ifelse(all(is.na(cartTotal)), 
+                                  NA, min(cartTotal, na.rm = TRUE))) %>%
+    mutate(ave_cartTotal = ifelse(all(is.na(cartTotal)), 
+                                  NA, mean(cartTotal, na.rm = TRUE))) %>%
+    mutate(clicks_per_sec = clickCount / duration) %>%
+    mutate(ave_item_cost = max(cartTotal) / max(cartCount)) %>%
+    mutate_if(
+      ~any(is.na(.)), 
+      ~ifelse(all(is.na(.)), NA, max(., na.rm = TRUE))) %>%
+    filter(row_id == max(row_id)) %>%
+    ungroup() %>%
+    select(-row_id)
+}
+
 fix_customerid <- function(data) {
   data %>%
     mutate(customerID = ifelse(is.na(customerID), 0, customerID))
